7c192dce417cc429f56fd7db521a43062f8505d7
Michelle Staten <mnstaten@yahoo.com>
2019-07-24 16:37:31 -0500

    initial commit

---
+++ src/main/java/com/staten/capstone/CapstoneApplication.java
@@ -0,0 +1,15 @@
+package com.staten.capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class CapstoneApplication {
+
+	public static void main(String[] args) {
+		run(CapstoneApplication.class, args);
+	}
+
+}
